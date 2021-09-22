f537e19206decce9ec6144cfa90b82d7f5c69f64
jsalaiz <jonathan.salaiz@gmail.com>
2021-09-21 20:52:12 -0400

    Completed the assignment

---
+++ BankEncapsulation/BankEncapsulation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userBankAccount = new BankAccount();
+            string userInput;
+            
+            do
+            {
+                Console.WriteLine("To get your balance type: balance To deposit money type: deposit To exit type: exit");
+                userInput = Console.ReadLine().ToUpper();
+
+                if (userInput == "BALANCE")
+                {
+                    Console.WriteLine($"Your current balance is: {userBankAccount.GetBalance()}");
+                }
+                else if (userInput == "DEPOSIT")
+                {
+                    Console.WriteLine("Enter an amount you would like to deposit: ");
+                    double depositAmount = Convert.ToDouble(Console.ReadLine());
+                    userBankAccount.Deposit(depositAmount);
+                }
+
+            }while (userInput != "EXIT");
+        }
+    }
+}
